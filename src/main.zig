e09eb8b935395dd06af63ba72eafe08cfe019dad
Rob <robertazzopardi@icloud.com>
2024-08-25 22:38:49 +0100

    Refactored out http components and up to the request header level

--- src/main.zig
+++ src/main.zig
@@ -1,7 +1,12 @@
 const std = @import("std");
+const r = @import("root.zig");
 
 const posix = std.posix;
 const net = std.net;
+const ArrayList = std.ArrayList;
+
+const CRLF = "\r\n";
+const SP = " ";
 
 const Server = struct {
     address: net.Address,
@@ -21,26 +26,125 @@ const Server = struct {
         };
     }
 
-    pub fn serve(self: *Server) !void {
+    fn serve(self: *Server) !void {
         var socklen = self.address.getOsSockLen();
 
         var buffer: [1024]u8 = undefined;
 
         while (true) {
-            const s = try posix.accept(self.socket, &self.address.any, &socklen, 0);
-            defer posix.close(s);
+            const connection = try posix.accept(self.socket, &self.address.any, &socklen, 0);
+            defer posix.close(connection);
 
-            const rec = try posix.recv(s, &buffer, 0);
-            std.debug.print("{s}\n", .{buffer[0..rec]});
+            const rec_size = try posix.recv(connection, &buffer, 0);
+            std.debug.print("{s}\n", .{buffer[0..rec_size]});
 
-            const res = "HTTP/1.0 200 OK\n\nHello World";
-            const rb = try posix.send(s, res, 0);
-            std.debug.print("Sent {d} bytes\n\n", .{rb});
+            _ = try Request.parse(buffer[0..rec_size]);
+
+            const server_response = "HTTP/1.0 200 OK\n\nHello World";
+            const sent_bytes = try posix.send(connection, server_response, 0);
+            std.debug.print("Sent {d} bytes\n\n", .{sent_bytes});
         }
 
         posix.close(self.socket);
     }
 };
+
+const RequestType = enum { simple, full };
+
+const Method = enum {
+    GET,
+    HEAD,
+    PUT,
+    POST,
+    DELETE,
+    LINK,
+    UNLINK,
+
+    const Error = error{not_supported};
+
+    fn string(self: Method) []const u8 {
+        return switch (self) {
+            Method.GET => "GET",
+            Method.HEAD => "HEAD",
+            Method.PUT => "PUT",
+            Method.POST => "POST",
+            Method.DELETE => "DELETE",
+            Method.LINK => "LINK",
+            Method.UNLINK => "UNLINK",
+        };
+    }
+
+    fn from(method: []const u8) Error!Method {
+        return std.meta.stringToEnum(Method, method) orelse Error.not_supported;
+    }
+};
+
+const Header = struct {
+    name: []const u8,
+    value: []const u8,
+
+    fn parse(line: []const u8) ?Header {
+        var parts = std.mem.split(u8, line, ":");
+        const name = parts.next() orelse return null;
+        const value = parts.next() orelse return null;
+        return .{
+            .name = name,
+            .value = value,
+        };
+    }
+};
+
+const RequestError = error{
+    InvalidRequest,
+};
+
+const RequestLine = struct {
+    method: Method,
+    resource: []const u8,
+    protocol: []const u8,
+
+    fn parse(line: []const u8) RequestError!RequestLine {
+        var part = std.mem.split(u8, line, SP);
+        const method_str = part.next() orelse return RequestError.InvalidRequest;
+        const method = Method.from(method_str) catch return RequestError.InvalidRequest;
+        const resource = part.next() orelse return RequestError.InvalidRequest;
+        const protocol = part.next() orelse return RequestError.InvalidRequest;
+
+        return .{
+            .method = method,
+            .resource = resource,
+            .protocol = protocol,
+        };
+    }
+};
+
+const Request = struct {
+    request_line: RequestLine,
+    headers: ArrayList(Header),
+
+    fn parse(request: []u8) RequestError!Request {
+        var lines = std.mem.split(u8, request, CRLF);
+        const request_line_string = lines.next() orelse return RequestError.InvalidRequest;
+        const request_line = try RequestLine.parse(request_line_string);
+
+        var headers = ArrayList(Header).init(std.heap.page_allocator);
+        while (lines.next()) |chunk| {
+            const header = Header.parse(chunk);
+
+            if (header) |val| {
+                headers.append(val) catch continue;
+                std.debug.print("{}\n", .{val});
+            }
+        }
+
+        return .{
+            .request_line = request_line,
+            .headers = headers,
+        };
+    }
+};
+
+const Response = struct {};
 
 pub fn main() !void {
     var server = try Server.new("127.0.0.1", 8000);
