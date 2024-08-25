f5afe863e1f9ceef3d0b9ce263d96fc768963f06
Rob <robertazzopardi@icloud.com>
2024-08-25 01:27:06 +0100

    Refactored code to a Server struct

--- src/main.zig
+++ src/main.zig
@@ -1,26 +1,49 @@
 const std = @import("std");
+
 const posix = std.posix;
+const net = std.net;
+
+const Server = struct {
+    address: net.Address,
+    socket: posix.socket_t,
+
+    fn new(ip: []const u8, port: u16) !Server {
+        var address = try net.Address.parseIp4(ip, port);
+        const socket = try posix.socket(posix.AF.INET, posix.SOCK.STREAM, posix.IPPROTO.TCP);
+
+        try posix.setsockopt(socket, posix.SOL.SOCKET, posix.SO.REUSEADDR, "true");
+        try posix.bind(socket, &address.any, address.getOsSockLen());
+        try posix.listen(socket, 1);
+
+        return .{
+            .address = address,
+            .socket = socket,
+        };
+    }
+
+    pub fn serve(self: *Server) !void {
+        var socklen = self.address.getOsSockLen();
+
+        var buffer: [1024]u8 = undefined;
+
+        while (true) {
+            const s = try posix.accept(self.socket, &self.address.any, &socklen, 0);
+            defer posix.close(s);
+
+            const rec = try posix.recv(s, &buffer, 0);
+            std.debug.print("{s}\n", .{buffer[0..rec]});
+
+            const res = "HTTP/1.0 200 OK\n\nHello World";
+            const rb = try posix.send(s, res, 0);
+            std.debug.print("Sent {d} bytes\n\n", .{rb});
+        }
+
+        posix.close(self.socket);
+    }
+};
 
 pub fn main() !void {
-    var sockaddr = try std.net.Address.parseIp4("127.0.0.1", 8080);
-    var socklen = sockaddr.getOsSockLen();
+    var server = try Server.new("127.0.0.1", 8000);
 
-    const socket = try posix.socket(posix.AF.INET, posix.SOCK.STREAM, posix.IPPROTO.TCP);
-    defer posix.close(socket);
-    try posix.setsockopt(socket, posix.SOL.SOCKET, posix.SO.REUSEADDR, "true");
-    try posix.bind(socket, &sockaddr.any, socklen);
-    try posix.listen(socket, 1);
-
-    var buffer: [1024]u8 = undefined;
-    while (true) {
-        const s = try posix.accept(socket, &sockaddr.any, &socklen, 0);
-        defer posix.close(s);
-
-        const rec = try posix.recv(s, &buffer, 0);
-        std.debug.print("Received {d} bytes: {s}\n", .{ rec, buffer[0..rec] });
-
-        const res = "HTTP/1.0 200 OK\n\nHello World";
-        const rb = try posix.send(s, res, 0);
-        std.debug.print("Sent {d} bytes\n\n", .{rb});
-    }
+    try server.serve();
 }
